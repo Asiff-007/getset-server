664554dce8a7d8fe577bbd0fb5081b57b4520f4a
vivek <vivekb@hashcube.com>
2021-08-09 23:13:30 +0530

    added db migration

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,14 +5,13 @@ var admin = require('../models/admin');
 module.exports = {
   create: function (req, resp) {
     var rules = {
-        
+        username: {type: 'string', required: true},
+        password: {type: 'string', required: true}
       };
 
     if (req.validate(rules)) {
       admin.login(req.body)
         .then(resp.success, resp.error);
     }
-    // console.log(req); 
-    // req.success = true;
   }
 };
