0afc6b65ca344fa8292e7d0e1762977aa7b5a762
Asiff-007 <asiffma1995@gmail.com>
2021-08-12 14:40:24 +0530

    commiting after created campaign table and campaign migration

---
+++ resources/migrations/20210812085341-campaign.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.up = function(db) {
+  return db.createTable('campaign', {
+    id:{type: 'int', primaryKey: true},
+    shop_id:{type: 'int'},
+    campaign_name: {type: 'string', length: 45},
+    from:{type:'date'},
+    to:{type:'date'},
+    admin_id:{type: 'int'},
+    status:{type:'boolean'}
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable('campaign');
+};
