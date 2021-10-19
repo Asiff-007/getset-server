4ee77765681978b36df61c903a2ac0029d391f70
Asiff-007 <asiffma1995@gmail.com>
2021-10-19 13:56:17 +0530

    commiting after add prize given ratio to sys config

--- models/game.js
+++ models/game.js
@@ -15,7 +15,8 @@ module.exports = new (Class({ //jshint ignore:line
     return price.getList(req)
       .then(function (priceList) {
         priceList = _.orderBy(priceList, ['expiry'], 'desc');
-        priceGivenRatio = sys_config.game_data.get(req.campaign_id).priceGivenRatio != null ? sys_config.game_data.get(req.campaign_id).priceGivenRatio : config.price_status.default_price_ratio;
+        priceGivenRatio = sys_config.game_data.get(req.campaign_id).price_given_ratio;
+        priceGivenRatio = priceGivenRatio ? priceGivenRatio : config.price_status.default_price_given_ratio;
 
         _.each(priceList, function (price, key) {
           var probability = expiryFactor * (price.count - price.given);
