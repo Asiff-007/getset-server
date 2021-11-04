5a455f61edbb9025f8c1198135c89637b840943f
Nasim Afeef <afeef.nasim@gmail.com>
2021-11-04 12:25:33 +0530

    analytics made as a module

--- games/lib/google_analytics.js
+++ games/lib/google_analytics.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBgEpR_BExdEkXN-hQZKC2LSsh9qIbB29w",
-    authDomain: "getset-for-shops.firebaseapp.com",
-    projectId: "getset-for-shops",
-    storageBucket: "getset-for-shops.appspot.com",
-    messagingSenderId: "733025079803",
-    appId: "1:733025079803:web:fc76e53ef4d25971f4fb5e",
-    measurementId: "G-MMMQEK1JEC"
-  };
+import {firebaseConfig} from '../sys_config.js';
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
