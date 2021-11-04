188a13b818d02521f8d2e7c742b052d8b4461648
Nasim Afeef <afeef.nasim@gmail.com>
2021-11-04 12:36:13 +0530

    changed name

--- games/lib/google_analytics.js
+++ games/lib/google_analytics.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
-import {firebaseConfig} from '../sys_config.js';
+import {firebaseConfig} from '../sys_front_end_config.js';
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
