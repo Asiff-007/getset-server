b530bd30dc2744f088b0c8a3d65090c1dbeef5fb
Nasim Afeef <afeef.nasim@gmail.com>
2021-11-04 12:25:33 +0530

    analytics made as a module

---
+++ games/sys_config_sample.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgEpR_BExdEkXN-hQZKC2LSsh9qIbB29w",
+    authDomain: "getset-for-shops.firebaseapp.com",
+    projectId: "getset-for-shops",
+    storageBucket: "getset-for-shops.appspot.com",
+    messagingSenderId: "733025079803",
+    appId: "1:733025079803:web:fc76e53ef4d25971f4fb5e",
+    measurementId: "G-MMMQEK1JEC"
+};
+
+export {firebaseConfig};
