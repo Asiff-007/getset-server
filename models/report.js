548a6a51933f928974310c3ae97f17ccd24b6a98
Asif ali <asifali@popcode.in>
2021-11-30 20:49:28 +0530

    pdf report generated

--- models/report.js
+++ models/report.js
@@ -12,8 +12,6 @@ module.exports = new (Class({ //jshint ignore:line
             claimedPrizes = [],
             mostClaimedPrizes = [],
             shops = [],
-            dayCounts = [],
-            timeCounts = [],
             mostActiveShop = [],
             mostPlayedTime,
             mostPlayedDay,
@@ -21,10 +19,20 @@ module.exports = new (Class({ //jshint ignore:line
             time = [],
             chartPlayers = [],
             chartClaimed = [],
-            days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            days = 
+            [
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday'
+            ],
             options = { weekday: 'long'},
             generatedOn,
-            h,h2,m,ampm1,ampm2;
+            prizeClaimed = config.price_status.claimed,
+            h,h2,ampm1,ampm2;
 
         return report.getData(id , date)
             .then(function(data){
@@ -35,42 +43,89 @@ module.exports = new (Class({ //jshint ignore:line
                     return data.priceId > 0;
                 }).length;
 
-                claimedPrizes = _.filter(data,['claimStatus',config.price_status.not_claimed]);
-                _.forIn(_.countBy(claimedPrizes,'priceName'), function(value, key) {
-                    mostClaimedPrizes.push({ "priceName": key, "count": value });
+                claimedPrizes = _.filter(data,
+                    [
+                        'claimStatus',config.price_status.not_claimed
+                    ]);
+                _.forIn(_.countBy(claimedPrizes,'priceName'), 
+                    function(value, key) {
+                        mostClaimedPrizes.push(
+                            {
+                                'priceName': key,
+                                'count': value
+                            });
                 });
 
                 _.forIn(data, function(value) {
-                    day.push({"day": new Intl.DateTimeFormat('en-US', options).format(value.playedDate), "status": value.claimStatus });
+                    var dayAt = new Intl.DateTimeFormat('en-US', options)
+                    .format(value.playedDate);
+
+                    var findDay = day.find(function(val){
+                        if( val.day === dayAt) {
+                            if (value.claimStatus === prizeClaimed) {
+                                val.claimed = val.claimed + 1;
+                            }
+                            val.players = val.players + 1;
+                            return true;
+                        }
+                        else {
+                            return false;
+                        }
+                     });
+                     if( !findDay ) {
+                         var claimed = 0;
+                         if (value.claimStatus === prizeClaimed) {
+                             claimed = 1;
+                         }
+                        day.push(
+                            {
+                                'day': dayAt,
+                                'players': 1,
+                                'claimed': claimed
+                            });
+                    }
+
                     h = value.playedDate.getHours();
-                    m = value.playedDate.getMinutes();
                     ampm1 = h >= 12 ? 'PM' : 'AM';
                     ampm2 = h >=11 && h < 23 ? 'PM' : 'AM';
                     h = h % 12 ;
                     h = h ? h : 12;
-                    h2 = h == 12 ? 0 : (h+1);
-                    time.push({"time" : h + ':00 '+ampm1+' - '+ h2 + ':00 '+ampm2});
+                    h2 = h === 12 ? 0 : (h+1);
+                    var timeAt = h + ':00 '+ampm1+' - '+ h2 + ':00 '+ampm2;
+
+                    var findTime = time.find(function(val){
+                       if( val.time === timeAt) {
+                            val.count = val.count + 1;
+                            return true;
+                       }
+                       else {
+                           return false;
+                       }
+                    });
+                    if( !findTime ) {
+                        time.push({'time': timeAt, 'count': 1});
+                    }
                 });
 
-                _.forIn(_.countBy(day,'day'), function(value, key) {
-                    var dayClaimed = _.filter(_.filter(day,['status',config.price_status.claimed]),['day' ,key]).length;
-                    dayCounts.push({ "day": key, "players": value ,"claimed":dayClaimed});
-                });
-
-                _.forIn(_.countBy(time,'time'), function(value, key) {
-                    timeCounts.push({ "time": key, "count": value });
-                });
-
-                mostPlayedDay = _.orderBy(dayCounts,['count'],'desc')[0];
-                mostPlayedTime = _.orderBy(timeCounts,['count'],'desc')[0];
+                mostPlayedDay = _.orderBy(day,['players'],'desc')[0];
+                mostPlayedTime = _.orderBy(time,['count'],'desc')[0];
 
                 _.forIn(_.countBy(data,'shopName'), function(value, key) {
-                    var shopClaimed = _.filter(claimedPrizes,['shopName',key]).length;
-                    shops.push({ "shopName": key, "players": value ,"claimed": shopClaimed});
+                    var shopClaimed = _.filter(claimedPrizes,
+                        [
+                            'shopName',key
+                        ])
+                        .length;
+                    shops.push(
+                        {
+                            'shopName': key,
+                            'players': value ,
+                            'claimed': shopClaimed
+                        });
                 });
 
-                _.forIn(days, function(value, key) {
-                    var d = _.filter(dayCounts,['day',value]);
+                _.forIn(days, function(value) {
+                    var d = _.filter(day,['day',value]);
                     if(d.length > 0) {
                         chartPlayers.push(d[0].players);
                         chartClaimed.push(d[0].claimed);
@@ -83,22 +138,26 @@ module.exports = new (Class({ //jshint ignore:line
                 });
 
                 mostActiveShop = _.orderBy(shops,['players'],'desc').slice(0,5);
-                mostClaimedPrizes = _.orderBy(mostClaimedPrizes,['count'],'desc').slice(0,5);
+                mostClaimedPrizes = _.orderBy(
+                    mostClaimedPrizes,
+                    ['count'],
+                    'desc')
+                    .slice(0,5);
 
                 generatedOn = new Date().toLocaleString();
                 
                 return {
-                    corporateName,
-                    totalPlayers,
-                    totalWinners,
-                    mostClaimedPrizes,
-                    mostPlayedDay,
-                    mostPlayedTime,
-                    mostActiveShop,
-                    chartPlayers,
-                    chartClaimed,
-                    generatedOn
+                    corporateName: corporateName,
+                    totalPlayers: totalPlayers,
+                    totalWinners: totalWinners,
+                    mostClaimedPrizes: mostClaimedPrizes,
+                    mostPlayedDay: mostPlayedDay,
+                    mostPlayedTime: mostPlayedTime,
+                    mostActiveShop: mostActiveShop,
+                    chartPlayers: chartPlayers,
+                    chartClaimed: chartClaimed,
+                    generatedOn: generatedOn
                 };
-            })
+            });
     }
 }))();
