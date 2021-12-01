cd34d06a07a9d6dc5a28a7ddb7d858c5999eb62d
Asif ali <asifali@popcode.in>
2021-12-01 14:38:15 +0530

    pdf report generated

--- resources/migrations/20211119183136-corporate.js
+++ resources/migrations/20211119183136-corporate.js
@@ -1,15 +1,22 @@
 'use strict';
 
-exports.up = function(db) {
+exports.up = function(db, callback) {
   return db.createTable('corporate', {
     id:{type: 'int', primaryKey: true, autoIncrement: true},
     name:{type: 'string', length: 40},
     address: {type: 'string', length: 80},
     email:{type:'string', length:320},
     status: {type: 'string', length:10}
-  });
+  },
+    function () {
+      db.addColumn('shop', 'corporate_id',
+        {type: 'int', unsigned: true}, callback);
+    });
 };
 
-exports.down = function(db) {
-  return db.dropTable('corporate');
+exports.down = function(db, callback) {
+  return db.dropTable('corporate',
+    function () {
+      db.removeColumn('shop', 'corporate_id', callback);
+    });
 };
