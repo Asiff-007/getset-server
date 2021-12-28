08a9f94cae20846aebbb847b044b6e273759c19b
Asif ali <asifali@popcode.in>
2021-12-28 23:37:08 +0530

    signup option added

--- controllers/enquiry.js
+++ controllers/enquiry.js
@@ -1,8 +1,7 @@
 'use strict';
 
 var enquiry = require('../models/enquiry');
-var config = require('../resources/config');
-var nodemailer = require('nodemailer');
+
 module.exports = {
   create:function (req, resp) {
     var rules = {
@@ -11,30 +10,8 @@ module.exports = {
       };
 
     if (req.validate(rules)) {
-      var transporter = nodemailer.createTransport({
-        service: config.report.service,
-        auth: {
-          user: config.report.user,
-          pass: config.report.password
-        }
-      });
-
-      var mailOptions = {
-        from: config.report.user,
-        to: config.report.user,
-        subject: config.report.subject,
-        text: 'Name  : ' + req.body.name +
-        '\nContact Number : ' + req.body.contact_number
-      };
-      transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-          enquiry.create(req.body)
-          .then(resp.success, resp.error);
-        }
-      });
+      enquiry.create(req.body)
+      .then(resp.success, resp.error);
     }
   }
 };
